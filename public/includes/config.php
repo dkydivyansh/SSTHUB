446ff8a50d64a91f7382bc6decf2723670e4f052
DKYDIVYANSH <164535367+dkydivyansh@users.noreply.github.com>
2026-09-05 16:00:50 +0530

    cleanup

--- public/includes/config.php
+++ public/includes/config.php
@@ -5,6 +5,6 @@ define('DB_PASS', '');
 define('DB_NAME', 'ssthub');
 
 // Google OAuth Credentials
-define('GOOGLE_CLIENT_ID', '395027667845-rnn22t43fi63jqoj6muqalemp1gt0ugs.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_CLIENT_ID', '');
+define('GOOGLE_CLIENT_SECRET', '');
 ?>
